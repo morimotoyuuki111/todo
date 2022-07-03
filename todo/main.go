d7672bb8014dbf9a88de280644dc17d0587deb45
morimotoyuuki111 <102967614+morimotoyuuki111@users.noreply.github.com>
2022-07-03 22:11:21 +0900

    Add files via upload

--- todo/main.go
+++ todo/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -85,14 +86,73 @@ func dbGetOne(id int) Todo {
 }
 
 
-
 func main() {
     router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+		router.LoadHTMLGlob("templates/*html")
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{})
-    })
+		dbInit()
 
-    router.Run()
+		//Index
+		router.GET("/", func(ctx *gin.Context) {
+				todos := dbGetAll()
+				ctx.HTML(200,"index.html", gin.H{
+						"todos": todos,
+				})
+		})
+
+		//Create
+		router.POST("/new", func(ctx *gin.Context){
+				text := ctx.PostForm("text")
+				status := ctx.PostForm("status")
+				dbInsert(text, status)
+				ctx.Redirect(302,"/")
+		})
+
+		//Detail
+		router.GET("/detail/:id",func(ctx *gin.Context){
+			n := ctx.Param("id")
+			id, err := strconv.Atoi(n)
+			if err !=nil {
+					panic(err)
+			}
+			todo := dbGetOne(id)
+			ctx.HTML(200,"detail.html", gin.H{"todo":todo})
+		})
+
+		//Update
+		router.POST("/update/:id", func(ctx *gin.Context){
+			n := ctx.Param("id")
+			id, err := strconv.Atoi(n)
+			if err != nil {
+					panic("ERROR")
+			}
+			text := ctx.PostForm("text")
+      status := ctx.PostForm("status")
+      dbUpdate(id, text, status)
+      ctx.Redirect(302, "/")
+		})
+
+		//削除機能
+		router.GET("/delete_check/:id",func(ctx *gin.Context){
+			n := ctx.Param("id")
+			id, err := strconv.Atoi(n)
+			if err != nil {
+					panic("ERROR")
+			}
+			todo := dbGetOne(id)
+			ctx.HTML(200, "delete.html",gin.H{"todo":todo})
+		})
+
+		//Delete
+		router.POST("/delete/:id", func(ctx *gin.Context){
+			n := ctx.Param("id")
+			id, err := strconv.Atoi(n)
+			if err != nil {
+					panic("ERROR")
+			}
+			dbDelete(id)
+			ctx.Redirect(302, "/")
+		})
+
+		router.Run()
 }
